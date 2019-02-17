0c6e3443687b3a22233543c31115d835326c5118
yancey <yancey1204@gmail.com>
2019-02-17 10:55:14 +0800

    order page with fake orderList data

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -8,6 +8,42 @@ Page({
    */
   data: {
     userInfo: null,
+    orderList: [
+      {
+        id: 0,
+        list: [{
+          count: 1,
+          image: 'cloud://yancey-ce9f04.7961-yancey-ce9f04/product1.jpg',
+          name: 'product1',
+          price: 50.5,
+        }]
+      },
+      {
+        id: 1,
+        list: [{
+          count: 1,
+          image: 'cloud://yancey-ce9f04.7961-yancey-ce9f04/product2.jpg',
+          name: 'product2',
+          price: 50.5,
+        },
+        {
+          count: 1,
+          image: 'cloud://yancey-ce9f04.7961-yancey-ce9f04/product3.jpg',
+          name: 'product3',
+          price: 50.5,
+        }
+        ]
+      },
+      {
+        id: 2,
+        list: [{
+          count: 1,
+          image: 'cloud://yancey-ce9f04.7961-yancey-ce9f04/product4.jpg',
+          name: 'product4',
+          price: 50.5,
+        }]
+      }
+    ], // orderList
   },
 
   onShow() {
