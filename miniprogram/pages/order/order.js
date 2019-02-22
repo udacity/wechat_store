a200fb844d542d44926b1169b8e053275f1b27b6
yancey <yancey1204@gmail.com>
2019-02-22 12:33:35 +0800

    resolve conflict

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -9,46 +9,7 @@ Page({
    */
   data: {
     userInfo: null,
-<<<<<<< HEAD
-    orderList: [
-      {
-        id: 0,
-        list: [{
-          count: 1,
-          image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
-          name: 'Product 1',
-          price: 50.5,
-        }]
-      },
-      {
-        id: 1,
-        list: [{
-          count: 1,
-          image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
-          name: 'Product 2',
-          price: 40.1,
-        },
-        {
-          count: 1,
-          image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
-          name: 'Product 3',
-          price: 30.5,
-        }
-        ]
-      },
-      {
-        id: 2,
-        list: [{
-          count: 1,
-          image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
-          name: 'Product 4',
-          price: 70.4,
-        }]
-      }
-    ], // orderList
-=======
     orderList: [],
->>>>>>> order page with get orders cloud function
   },
 
   onLoad() {
