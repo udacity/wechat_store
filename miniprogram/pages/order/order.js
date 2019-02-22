bd58c58394b1d995ae31d5ac3b4ddec3382ff0c6
Shiyu Yang <yangshiyu89@gmail.com>
2019-02-22 17:39:38 +0800

    Update order.js

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -10,7 +10,7 @@ Page({
     userInfo: null,
     orderList: [{
         id: 0,
-        list: [{
+        productList: [{
           count: 1,
           image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
           name: 'Product 1',
@@ -19,7 +19,7 @@ Page({
       },
       {
         id: 1,
-        list: [{
+        productList: [{
             count: 1,
             image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
             name: 'Product 2',
@@ -35,7 +35,7 @@ Page({
       },
       {
         id: 2,
-        list: [{
+        productList: [{
           count: 2,
           image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
           name: 'Product 4',
@@ -53,7 +53,7 @@ Page({
       }),
       // 2 digits for price
       this.data.orderList.forEach(order => {
-        order.list.forEach(product => product.price = util.priceFormat(product.price))
+        order.productList.forEach(product => product.price = util.priceFormat(product.price))
       })
 
     this.setData({
