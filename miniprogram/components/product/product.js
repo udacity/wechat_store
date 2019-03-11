a8ba4a357c0f57b81dc9c3bf08eb53c24e1b6e28
yangshiyu89 <yangshiyu89@sina.com>
2019-03-11 20:46:27 +0800

    Upload comment

---
+++ miniprogram/components/product/product.js
@@ -0,0 +1,25 @@
+// components/product/product.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    productImage: String,
+    productName: String,
+    productPrice: String
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/review/review.js
@@ -0,0 +1,28 @@
+// pages/review/review.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    product: {
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      name: 'Product 1',
+      price: '50.50',
+    },
+
+    reviewList: [{
+      avatar: '/images/me-sel.png',
+      username: 'test1',
+      createTime: '2019/01/01',
+      content: 'test comment',
+    },
+    {
+      avatar: '/images/me-sel.png',
+      username: 'test2',
+      createTime: '2019/01/01',
+      content: 'test comment'
+    }],
+
+  },
+})
